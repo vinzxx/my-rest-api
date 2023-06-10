4622e93eb91d7c98b591a8175540e4ac75536efa
vinzxx <101110589+vinzxx@users.noreply.github.com>
2023-06-11 01:56:04 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Vinz Host'// yourname
+global.MONGO_DB_URI = "mongodb+srv://vinzhost:vinzhost123@cluster0.xd1echw.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "vinzhoost@gmail.com" //email
+global.email_password = "ygpmehgcbyrqjxhm" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
